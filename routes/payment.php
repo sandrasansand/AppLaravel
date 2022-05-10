704eb06800e4a4e2803b5f5764633f09cc4a86c0
Sandra Sáez <sandrasaezcarbonell@gmail.com>
2022-05-11 01:48:55 +0200

    first commit respaldo 2 app

---
+++ routes/payment.php
@@ -0,0 +1,8 @@
+<?php  
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaymentController;
+
+Route::get('{course}/checkout',[PaymentController::class, 'checkout'])->name('checkout');
+
+Route::get('{course}/pay',[PaymentController::class, 'pay'])->name('pay');
