9cbaa87010cbfe6382794e03e667104c21fd92c2
Sandra Sáez <sandrasaezcarbonell@gmail.com>
2022-05-11 02:22:24 +0200

    first commit respaldo 2 app

---
+++ app/Models/Observation.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Observation extends Model
+{
+    use HasFactory;
+    protected $fillable=['body', 'course_id']; //asignación masiva
+
+    //rel 1:1 inversa
+    public function course(){
+        return $this->belongsTo('App\Models\Course');
+    }
+}
