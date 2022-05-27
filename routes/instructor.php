155a7c818f8bbd33147c46d8253aed27463b8f33
sandrasansand <sandrasaezcarbonell@gmail.com>
2022-05-27 15:13:52 +0200

    update route instructor
    8th commit update routes students and curriculum

--- routes/instructor.php
+++ routes/instructor.php
@@ -19,13 +19,12 @@ Route::redirect('', 'instructor/courses');
 Route::resource('courses', CourseController::class)->names('courses');
 
 //comp liv controla la ruta invocar render()
-Route::get('courses/{course}/curriculum', [CoursesCurriculum::class])->middleware('can:Actualizar cursos')->name('courses.curriculum');
+Route::get('courses/{course}/curriculum', CoursesCurriculum::class)->middleware('can:Actualizar cursos')->name('courses.curriculum');
 
 Route::get('courses/{course}/goals', [CourseController::class, 'goals'])->name('courses.goals');
 
-Route::get('courses/{course}/students',[ CoursesStudents::class])->middleware('can:Actualizar cursos')->name('courses.students');
+Route::get('courses/{course}/students', CoursesStudents::class)->middleware('can:Actualizar cursos')->name('courses.students');
 
 Route::post('courses/{course}/status', [CourseController::class, 'status'])->name('courses.status');
 
 Route::get('courses/{course}/observation', [CourseController::class, 'observation'])->name('courses.observation');
-
